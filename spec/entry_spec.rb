9aa6734e13bd4643993a9a06db3adb0cdc30d29b
JohnZiv <jovan.z@elfak.rs>
2016-05-06 21:24:10 -0400

    modifies entry.rb and entry_spec.rb

---
+++ spec/entry_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../models/entry.rb'
+
+RSpec.describe Entry do
+	context "attributes" do
+		it "should respond to name" do
+			entry = Entry.new
+			expect(entry)to respond_to(:name)
+		end
+		it "should respond to phone_number" do
+			entry = Entry.new
+			expect(entry)to respond_to(:phone_number)
+		end
+		it "should respond to email" do
+			entry = Entry.new
+			expect(entry)to respond_to(:email)
+		end
+	end
+end
