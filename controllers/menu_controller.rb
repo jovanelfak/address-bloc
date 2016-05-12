2bc260ccdbff706f4d2f996daaf71749ab245f69
JohnZiv <jovan.z@elfak.rs>
2016-05-11 21:40:44 -0400

    Added submenu implementation

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -18,7 +18,67 @@ class MenuController
 		print "Enter your selection: "
 
 		selection = gets.to_i
-		puts "You picked #{selection}"
+		case selection
+		when 1
+			system "clear"
+			view_all_entrieschi
+			main_menu
+		when 2
+			system "clear"
+			create_entry
+			main_menu
+		when 3
+			system "clear"
+			create_entry
+			main_menu
+		when 4
+			system "clear"
+			read_csv
+			main_menu
+		when 5
+			puts "Good-bye!"
+			exit (0)
+		else
+			system "clear"
+			puts "Sorry, that is not a valid input"
+			main_menu
+		end
+	end
+
+	def view_all_entries
+		address_book.entries.each do |x|
+			system "clear"
+			puts x.to_s
+
+			entry_submenu(entry)
+		end
+		system "clear"
+		puts "End of entries"
+
+	end
+
+	def create_entry
+		system "clear"
+		puts "New AddressBloc Entry"
+		print "Name: "
+		name = gets.chomp
+		print "Phone number: "
+		phone = gets.chomp
+		print "Email: "
+		email = gets.chomp
+
+		address_book.add_entry(name, phone_number, email)
+
+		system "clear"
+		puts "New entry created"
+	end
+
+	def search_entries
+
+	end
+
+	def read_csv
+
 	end
 
 end
